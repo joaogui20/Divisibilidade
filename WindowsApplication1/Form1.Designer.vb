1dbacbe0960ba6d6dd985eec4ce3fb809f2d0d49
João Guilherme <azazazazaisjoao@gmail.com>
2023-08-08 10:10:32 -0400

    Projeto Divisibilidade no Visual Basic .NET

---
+++ WindowsApplication1/Form1.Designer.vb
@@ -0,0 +1,89 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmDivisibilidade
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.lblTexto = New System.Windows.Forms.Label()
+        Me.txtNumero = New System.Windows.Forms.TextBox()
+        Me.btnCalcular = New System.Windows.Forms.Button()
+        Me.lstResultado = New System.Windows.Forms.ListBox()
+        Me.SuspendLayout()
+        '
+        'lblTexto
+        '
+        Me.lblTexto.AutoSize = True
+        Me.lblTexto.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblTexto.Location = New System.Drawing.Point(12, 9)
+        Me.lblTexto.Name = "lblTexto"
+        Me.lblTexto.Size = New System.Drawing.Size(285, 20)
+        Me.lblTexto.TabIndex = 0
+        Me.lblTexto.Text = "Calcula a divisibilidade até o número 20"
+        '
+        'txtNumero
+        '
+        Me.txtNumero.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtNumero.Location = New System.Drawing.Point(16, 33)
+        Me.txtNumero.Name = "txtNumero"
+        Me.txtNumero.Size = New System.Drawing.Size(279, 26)
+        Me.txtNumero.TabIndex = 1
+        '
+        'btnCalcular
+        '
+        Me.btnCalcular.Location = New System.Drawing.Point(16, 66)
+        Me.btnCalcular.Name = "btnCalcular"
+        Me.btnCalcular.Size = New System.Drawing.Size(279, 40)
+        Me.btnCalcular.TabIndex = 2
+        Me.btnCalcular.Text = "Calcular"
+        Me.btnCalcular.UseVisualStyleBackColor = True
+        '
+        'lstResultado
+        '
+        Me.lstResultado.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lstResultado.FormattingEnabled = True
+        Me.lstResultado.ItemHeight = 20
+        Me.lstResultado.Location = New System.Drawing.Point(16, 113)
+        Me.lstResultado.Name = "lstResultado"
+        Me.lstResultado.Size = New System.Drawing.Size(281, 224)
+        Me.lstResultado.TabIndex = 3
+        '
+        'frmDivisibilidade
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.SystemColors.ActiveCaption
+        Me.ClientSize = New System.Drawing.Size(307, 363)
+        Me.Controls.Add(Me.lstResultado)
+        Me.Controls.Add(Me.btnCalcular)
+        Me.Controls.Add(Me.txtNumero)
+        Me.Controls.Add(Me.lblTexto)
+        Me.Name = "frmDivisibilidade"
+        Me.Text = "Divisibilidade"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents lblTexto As System.Windows.Forms.Label
+    Friend WithEvents txtNumero As System.Windows.Forms.TextBox
+    Friend WithEvents btnCalcular As System.Windows.Forms.Button
+    Friend WithEvents lstResultado As System.Windows.Forms.ListBox
+
+End Class
---
+++ WindowsApplication1/Form1.vb
@@ -0,0 +1,43 @@
+﻿Public Class frmDivisibilidade
+
+    Private Function IsDivididoPor(Numero As Long, DivNumero As Long) As Boolean
+
+        If DivNumero <> 0 Then
+            Return Numero Mod DivNumero = 0
+        End If
+
+        Return False
+
+    End Function
+
+    Private Sub btnCalcular_Click(sender As Object, e As EventArgs) Handles btnCalcular.Click
+        lstResultado.Items.Clear()
+
+        Dim numero As Integer = Convert.ToInt32(txtNumero.Text)
+
+        Dim valor(20) As Boolean
+
+        For x = 1 To 20
+            valor(x) = IsDivididoPor(numero, x)
+        Next
+
+        If valor.Any(Function(x) x.Equals(False)) Then
+            lstResultado.Items.Add(numero & " é divisível por: ")
+        End If
+
+        For y = 1 To 20
+            If valor(y) Then
+                lstResultado.Items.Add(y)
+            End If
+        Next
+    End Sub
+
+    Private Sub txtNumero_KeyPress(sender As Object, e As KeyPressEventArgs) Handles txtNumero.KeyPress
+        If Not Char.IsDigit(e.KeyChar) Then
+            e.Handled = True
+            If (Asc(e.KeyChar) = 8) Then
+                e.Handled = False
+            End If
+        End If
+    End Sub
+End Class
